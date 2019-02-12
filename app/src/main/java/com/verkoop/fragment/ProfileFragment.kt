35ae600bc2738c9325d3742334fe6f2a7eb04352
Anmol Gupta <anmol@mobilecoderz.com>
2019-02-12 19:17:11 +0530

    cahtegories api login api set up

---
+++ app/src/main/java/com/verkoop/fragment/ProfileFragment.kt
@@ -0,0 +1,5 @@
+package com.verkoop.fragment
+
+/**
+ * Created by intel on 08-02-2019.
+ */
---
+++ app/src/main/java/com/verkoop/network/ApiClient.java
@@ -0,0 +1,8 @@
+package com.verkoop.network;
+
+/**
+ * Created by intel on 12-02-2019.
+ */
+
+public class ApiClient {
+}
